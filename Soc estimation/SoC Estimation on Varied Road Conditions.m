fea4f27f831f9f7cc8676125c819cd9fe8bb7717
SRUJAN M N <srujanhemraj@gmail.com>
2024-11-26 19:12:54 +0530

    Update SoC Estimation on Varied Road Conditions.m

--- Soc estimation/SoC Estimation on Varied Road Conditions.m
+++ Soc estimation/SoC Estimation on Varied Road Conditions.m
@@ -1,4 +1,24 @@
-SoC = SoC - (energyUsed / batteryCapacity) * 100; % Update SoC
+%% Simulate Battery Management System (BMS) for EV on Different Road Types
+% Version: 1.0
+% Description: This script simulates the State of Charge (SoC) reduction of an EV battery
+% based on distances traveled on various road types with different energy consumption rates.
+
+%% Initial Parameters
+initialSoC = 100; % Initial State of Charge in percentage
+batteryCapacity = 75; % Battery Capacity in kWh
+roadTypes = {'Highway', 'Muddy Road', 'Village Road', 'City Traffic Road'}; % Road categories
+roadConsumptionRates = [0.15, 0.3, 0.25, 0.4]; % Energy consumption (kWh/km) for each road type
+distanceTraveled = [50, 20, 30, 15]; % Distance traveled on each road type (in km)
+
+%% Initialize Variables
+SoC = initialSoC; % Current State of Charge
+socHistory = [SoC]; % Record of SoC at each stage
+
+%% Simulate SoC Reduction
+for i = 1:length(roadTypes)
+    % Calculate energy used and update SoC
+    energyUsed = roadConsumptionRates(i) * distanceTraveled(i); % Energy used on this road
+    SoC = SoC - (energyUsed / batteryCapacity) * 100; % Update SoC
     SoC = max(SoC, 0); % Ensure SoC doesn't go below 0
     socHistory(end+1) = SoC; % Append to history
 end
