1614f7b0cae1c0065409edfcfb76669a404f2a70
SRUJAN M N <srujanhemraj@gmail.com>
2024-11-26 19:11:19 +0530

    Update SoC Estimation on Varied Road Conditions.m

--- Soc estimation/SoC Estimation on Varied Road Conditions.m
+++ Soc estimation/SoC Estimation on Varied Road Conditions.m
@@ -1,1 +1,20 @@
-%
+SoC = SoC - (energyUsed / batteryCapacity) * 100; % Update SoC
+    SoC = max(SoC, 0); % Ensure SoC doesn't go below 0
+    socHistory(end+1) = SoC; % Append to history
+end
+
+%% Visualize Results
+figure;
+bar(categorical(['Start', roadTypes]), socHistory, 'FaceColor', [0.2, 0.7, 0.8]); % Bar chart
+xlabel('Road Types');
+ylabel('State of Charge (SoC) %');
+title('Battery SoC on Different Road Conditions');
+grid on;
+
+% Annotate SoC values on the chart
+for i = 1:length(socHistory)
+    text(i, socHistory(i) + 1, sprintf('%.2f%%', socHistory(i)), 'HorizontalAlignment', 'center');
+end
+
+%% Display Completion Message
+disp('Simulation Complete. Check the plot for SoC visualization.');
